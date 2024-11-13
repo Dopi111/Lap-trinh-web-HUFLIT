3f351937b06aef8a3ea7484084d8d2412af6828a
HuyLamIT <huylam167@gmail.com>
2024-11-13 09:25:36 +0700

    addd

--- Login/Login/Controllers/AccountController.cs
+++ Login/Login/Controllers/AccountController.cs
@@ -10,56 +10,68 @@ namespace Login.Controllers
     public class AccountController : Controller
     {
         UserData1Entities db = new UserData1Entities();
+
         // GET: Account
-        public ActionResult Index()
-        {
-            return View();
-        }
         [HttpGet]
         public ActionResult Register()
         {
             return View();
-
         }
+
+        // POST: Account/Register
         [HttpPost]
+        
         public ActionResult Register(UserRegister ur)
         {
             if (ModelState.IsValid)
             {
+                // Kiểm tra trùng lặp UserName
+                if (db.UserRegister.Any(x => x.UserName == ur.UserName))
+                {
+                    ViewBag.ErrorMessage = "User name already exists. Please choose a different user name.";
+                    return View();
+                }
+
+                // Kiểm tra trùng lặp Email
                 if (db.UserRegister.Any(x => x.Email == ur.Email))
                 {
-                    ViewBag.Mesager = "Email already registered";
-                }
-                else
-                {
-                    db.UserRegister.Add(ur);
-                    db.SaveChanges();
-                    Response.Write("<script>alert('Registration successful')<script>");
-
+                    ViewBag.ErrorMessage = "Email already registered. Please use a different email address.";
+                    return View();
                 }
 
+                // Nếu không trùng, thêm người dùng mới vào cơ sở dữ liệu
+                db.UserRegister.Add(ur);
+                db.SaveChanges();
+
+                // Gửi thông báo thành công vào TempData
+                TempData["SuccessMessage"] = "Registration successful! You will be redirected to the login page.";
+
+                // Chuyển hướng đến trang Login
+                return RedirectToAction("Login", "Account");
             }
             return View();
         }
+
         [HttpGet]
         public ActionResult Login()
         {
             return View();
-
         }
+
         [HttpPost]
         public ActionResult Login(MyLogin l)
         {
             var query = db.UserRegister.SingleOrDefault(m => m.Email == l.Email && m.Password == l.Password);
             if (query != null)
             {
-                Response.Write("<script>alert('Login successful')<script>");
+                // Thực hiện hành động sau khi đăng nhập thành công
+                return RedirectToAction("Index", "Home");
             }
             else
             {
-                Response.Write("<script>alert('Invalid Credentials')<script>");
+                ViewBag.ErrorMessage = "Invalid Credentials";
+                return View();
             }
-            return View();
         }
     }
 }
